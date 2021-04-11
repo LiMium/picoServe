23e100bfa2027ae8c20a5201741e042e4cf1d24b
hrj <harshad.rj@gmail.com>
2021-04-11 16:15:44 +0530

    test with loom

--- src/main/java/org/limium/picoServe/Server.java
+++ src/main/java/org/limium/picoServe/Server.java
@@ -107,10 +107,20 @@ public final class Server {
 
   public static void main(String[] args) throws java.io.IOException {
     System.out.println("Starting");
-    var server = Server.builder()
+    final var executor = new java.util.concurrent.ForkJoinPool();
+
+    // Can be used in Project Loom
+    // final var executor = java.util.concurrent.Executors.newVirtualThreadExecutor();
+
+    final var server = Server.builder()
       .port(9000)
       .backlog(5)
+      .executor(executor)
       .handle(new Handler("/string", () -> {
+        return new StringResponse(200, "hello", Map.of("Content-type", List.of("text/plain")));
+      }))
+      .handle(new Handler("/stringWithDelay", () -> {
+        try { Thread.sleep(10); } catch (java.lang.InterruptedException e) { System.out.println("Interrupted");}
         return new StringResponse(200, "hello", Map.of("Content-type", List.of("text/plain")));
       }))
       .handle(new Handler("/bytes", () -> { return new ByteResponse(200, new byte[] {0x11, 0x22, 0x33}); }))
